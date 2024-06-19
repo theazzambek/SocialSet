c6ab6067d22f3a8d59702b983461156143ada45c
unknown <masterkak11110@gmail.com>
2024-06-19 16:27:27 +0600

    Add

--- socialset/socialset/urls.py
+++ socialset/socialset/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-urlpatterns = [
+
+urlpatterns = *i18n_patterns[
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path("socialnetwork/",include("socialnetwork.urls")),
